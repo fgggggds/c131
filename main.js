f86a20de5e16b136ec64cf99fefa21c3ddd269c1
fgggggds <136881575+fgggggds@users.noreply.github.com>
2023-06-17 12:09:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,19 +20,19 @@ var previous_result = '';
 function gotResult(error, results){
   if (error) {
     console.log(error);
-  } else{
+  } else{ 
     if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
       console.log(results);
       previous_result = results[0].label;
-      var synth = windows.speechSynthesis;
+      var synth = window.speechSynthesis;
       speak_data = 'Object detected is - '+results[0].label;
       var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
 
       document.getElementById("result_object_name").innerHTML = results[0].label;
       document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-
     }
+    
   } 
 
 }
